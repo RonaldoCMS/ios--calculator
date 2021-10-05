80d7f5f4ebe7d3d3841407753245f3de1a81c843
RonaldoCMS <58113248+RonaldoCMS@users.noreply.github.com>
2021-10-05 22:19:25 +0200

    not work

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+// ignore_for_file: import_of_legacy_library_into_null_safe
+// @dart=2.9
+
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -13,103 +18,171 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class HomePage extends StatefulWidget {
+  const HomePage({Key key, @required this.title}) : super(key: key);
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Calculator(),
+    );
   }
+}
+
+class Calculator extends StatefulWidget {
+  const Calculator({Key key}) : super(key: key);
+
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  String value = "0";
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return SafeArea(
+        child: Container(
+      padding: EdgeInsets.all(8),
+      color: Colors.black,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 25),
+          Container(
+            alignment: Alignment.bottomRight,
+            child: FittedBox(
+              child: AutoSizeText(
+                value,
+                textAlign: TextAlign.end,
+                minFontSize: 80,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 25),
+          Row(
+            children: [
+              button(
+                "DEL",
+                Colors.black,
+                Colors.grey,
+              ),
+              SizedBox(width: 10),
+              button(
+                "CANC",
+                Colors.black,
+                Colors.grey,
+              ),
+              SizedBox(width: 10),
+              button("%", Colors.black, Colors.grey),
+              SizedBox(width: 10),
+              button(
+                "/",
+                Colors.black,
+                Colors.orange,
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              button("7", Colors.black, Color(0xFF3e3f42)),
+              SizedBox(width: 10),
+              button("8", Colors.black, Color(0xFF3e3f42)),
+              SizedBox(width: 10),
+              button("9", Colors.black, Color(0xFF3e3f42)),
+              SizedBox(width: 10),
+              button("*", Colors.black, Colors.orange),
+            ],
+          ),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              button("4", Colors.black, Color(0xFF3e3f42)),
+              SizedBox(width: 10),
+              button("5", Colors.black, Color(0xFF3e3f42)),
+              SizedBox(width: 10),
+              button("6", Colors.black, Color(0xFF3e3f42)),
+              SizedBox(width: 10),
+              button("-", Colors.black, Colors.orange),
+            ],
+          ),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              button("1", Colors.black, Color(0xFF3e3f42)),
+              SizedBox(width: 10),
+              button("2", Colors.black, Color(0xFF3e3f42)),
+              SizedBox(width: 10),
+              button("3", Colors.black, Color(0xFF3e3f42)),
+              SizedBox(width: 10),
+              button("+", Colors.black, Colors.orange),
+            ],
+          ),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              Expanded(child: button("0", Colors.black, Color(0xFF3e3f42))),
+              SizedBox(width: 10),
+              button(".", Colors.black, Color(0xFF3e3f42)),
+              SizedBox(width: 10),
+              button("=", Colors.black, Colors.orange),
+            ],
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    ));
+  }
+
+  Widget button(value, colorValue, color) {
+    return InkWell(
+      child: Container(
+        alignment: Alignment.center,
+        height: 90,
+        width: 90,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(100),
+        ),
+        child: AutoSizeText(
+          value.toString(),
+          minFontSize: 30,
+          textAlign: TextAlign.center,
+          style: TextStyle(color: colorValue),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      onTap: () {
+        setState(() {
+          if (value == "=") {
+            Parser p = new Parser();
+            Expression exp = p.parse(this.value);
+            this.value = exp.evaluate(EvaluationType.REAL, null).toString();
+          } else if (value == "DEL") {
+            this.value = "0";
+          } else if (value == "0") {
+            this.value = value;
+          } else {
+            this.value += value;
+          }
+        });
+      },
     );
   }
 }
